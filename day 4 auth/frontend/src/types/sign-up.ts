07fc9a5595c5bf8738da8c9149fd7044d6266dcf
Sumit Kumar <sumit.1183@zenmonk.tech>
2026-03-02 09:46:02 +0530

    error-msg in  type-schema  included

--- day 4 auth/frontend/src/types/sign-up.ts
+++ day 4 auth/frontend/src/types/sign-up.ts
@@ -1,9 +1,13 @@
 import * as z from "zod";
 
 const SignUpSchema = z.object({
-    name: z.string().min(3).max(20),
-    email: z.string().min(5).max(50).email("Invalid email address"),
-    password: z.string().min(1).max(20),
+    name: z.string()
+        .min(3, { message: "Name must be at-least 3 characters" })
+        .max(20, { message: "Name cannot exceed 20 characters" }),
+    email: z.string().email("Invalid email address"),
+    password: z.string()
+        .min(8, { message: "Password must be at-least 8 characters" })
+        .max(20, { message: "Password cannot exceed 20 characters" }),
 });
 
 type SignupInterface = z.Infer<typeof SignUpSchema>
